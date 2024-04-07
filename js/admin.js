3298fb2d930e43fa3ce40fab2738b99218a42977
rameezwp <rameez7759@gmail.com>
2024-04-07 11:18:03 +0500

    initial-commit-from-wp

---
+++ js/admin.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+	$('.spinner').css('visibility', 'visible');
+	$('.spinner').hide();
+	$('.success-text').hide();
+	$('.woope-form').submit(function(event) {
+		event.preventDefault();
+		var formData = $(this).serialize();
+		$('.spinner').show();
+		$('.success-text').hide();
+		$.post(ajaxurl, formData, function(resp) {
+			$('.spinner').hide();
+			$('.success-text').text(resp);
+			$('.success-text').show();
+		});
+	});
+});
